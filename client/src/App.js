99c45f96540afefe269e780f476b97b782d4b359
Babalwa01 <babalwambolekwa93@gmail.com>
2022-01-15 07:49:39 +0200

    frontend setup

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Wealth Builders Chats</h1>
+    </div>
+  )
+}
+
+export default App;
+
